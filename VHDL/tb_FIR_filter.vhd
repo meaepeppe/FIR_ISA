2a9b56e345219f6cb911c769fd666a44ba50fd16
Giuseppe B <meaepeppe@gmail.com>
2017-10-20 17:08:34 +0200

    Add files via upload

--- VHDL/tb_FIR_filter.vhd
+++ VHDL/tb_FIR_filter.vhd
@@ -13,22 +13,29 @@ END ENTITY;
 
 ARCHITECTURE test OF tb_FIR_filter IS
 
+	TYPE vector_test IS ARRAY (N_sample-1 DOWNTO 0) OF INTEGER;
+	TYPE coeffs_array IS ARRAY (N DOWNTO 0) OF INTEGER;
+	TYPE sig_array IS ARRAY (N DOWNTO 0) OF SIGNED(Nb-1 DOWNTO 0);
+
+	FILE inputs: text;
+	FILE coeff_file: text;
+	SHARED VARIABLE input_samples: vector_test;
+
 	SIGNAL CLK, RST_n: STD_LOGIC;
 	SIGNAL VIN, VOUT: STD_LOGIC;
 	SIGNAL sample, filter_out: SIGNED(Nb-1 DOWNTO 0);
-	SIGNAL coeffs: SIGNED(((N+1)*Nb)-1 DOWNTO 0);
-	
-	FILE inputs: text;
-	
-	TYPE vector_test IS ARRAY (N_sample-1 DOWNTO 0) OF INTEGER;
-	TYPE sig_array IS ARRAY (N DOWNTO 0) OF SIGNED(Nb-1 DOWNTO 0);
-	
-	VARIABLE input_samples: vector_test;
+	SIGNAL DINconverted,filter_outconverted: STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
+	SIGNAL coeffs_std: std_logic_vector ((N+1)*Nb - 1 DOWNTO 0);
+
+
 	
 BEGIN
 
+DINconverted <= std_logic_vector(sample);
+filter_outconverted <= std_logic_vector(filter_out);
+
 DUT: ENTITY WORK.FIR_filter GENERIC MAP (N,Nb) 
-	PORT MAP (CLK => CLK, RST_n => RST_n, VIN => VIN, std_logic_vector(DIN) => sample, std_logic_vector(coeffs) => coeffs, VOUT => VOUT, DOUT => filter_out);
+	PORT MAP (CLK => CLK, RST_n => RST_n, VIN => VIN, DIN => DINconverted,  Coeffs => coeffs_std, VOUT => VOUT, DOUT => filter_outconverted);
 	
 	CLK_gen: PROCESS
 	BEGIN
@@ -41,21 +48,36 @@ DUT: ENTITY WORK.FIR_filter GENERIC MAP (N,Nb)
 	test_input_read: PROCESS
 	VARIABLE iLine: LINE;
 	VARIABLE i: INTEGER := 0;
-	RST_n => '0';
-	VIN => '0';
+	VARIABLE coeffs_integer: coeffs_array;
+
 	BEGIN
-		file_open(inputs, "C:\Users\anton\Desktop\ISA\Labs1718\ISA_Lab1\VHDL\QUARTUS\input_vectors.txt", READ_MODE);
+	RST_n <= '0';
+	VIN <= '0';
+		file_open(inputs, "input_vectors.txt", READ_MODE);
 		WHILE (NOT ENDFILE(inputs)) LOOP
 			READLINE(inputs, iLine);
 			READ(iLine, input_samples(i));
 			i := i+1;
+		END LOOP;
+		file_close(inputs);
+
+		file_open(coeff_file, "coeffs.txt", READ_MODE);
+		WHILE (NOT ENDFILE(coeff_file)) LOOP
+			READLINE(coeff_file, iLine);
+			READ(iLine, coeffs_integer(i));
+			
+		END LOOP;
+		file_close(coeff_file);
+		
+		FOR i IN 0 TO N LOOP
+			coeffs_std((i+1)*Nb-1 DOWNTO i*Nb)<= std_logic_vector(to_signed(coeffs_integer(i),Nb));
 		END LOOP;
 		
 		WAIT FOR 10 ns;
 		RST_n <= '1';
 		WAIT FOR 5 ns;
 		VIN <= '1';
-		file_close(inputs);
+		
 		WAIT;
 	
 	END PROCESS;
@@ -63,21 +85,21 @@ DUT: ENTITY WORK.FIR_filter GENERIC MAP (N,Nb)
 	test_results_write: PROCESS(CLK)
 	VARIABLE oLine: LINE;
 	VARIABLE i: INTEGER := 0;
-	FILE results: text is out "C:\Users\anton\Desktop\ISA\Labs1718\ISA_Lab1\VHDL\QUARTUS\output_vectors.txt"
+	FILE results: text is out "output_vectors.txt";
 	BEGIN
 		
 		IF CLK'EVENT AND CLK = '1' AND VIN = '1' THEN
-			sample <= input_samples(i);
+			sample <= to_signed(input_samples(i),sample'LENGTH);
 			WRITE(oLine, to_integer(sample));
 			WRITELINE(results, oLine);
-			i = i+1;
+			i := i+1;
 			IF i = N_sample-1 THEN
 				VIN <= '0';
 			END IF;
 		END IF;
 		
 		IF VIN = '0' THEN
-			i = 0;
+			i := 0;
 		END IF;
 		
 	END PROCESS;
