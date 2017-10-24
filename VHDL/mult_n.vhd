d958b1b5728f63a75abe8d8745026ee547a9b233
Giuseppe B <meaepeppe@gmail.com>
2017-10-24 16:52:13 +0200

    Mult extended

--- VHDL/mult_n.vhd
+++ VHDL/mult_n.vhd
@@ -9,16 +9,16 @@ ENTITY mult_n IS
 	PORT(
 		in_a: IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
 		in_b: IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
-		mult_out: OUT STD_LOGIC_VECTOR(Nb-1 DOWNTO 0)
+		mult_out: OUT STD_LOGIC_VECTOR(2*Nb-1 DOWNTO 0)
 	);
 END ENTITY;
 
-ARCHITECTURE beh OF mult_n IS
+ARCHITECTURE beh_mult OF mult_n IS
 	SIGNAL mult_signed: SIGNED((2*Nb)-1 DOWNTO 0);
 BEGIN
 	multiplication: PROCESS(in_a, in_b)
 	BEGIN
 		mult_signed <= SIGNED(in_a) * SIGNED(in_b);
 	END PROCESS;
-	mult_out <= STD_LOGIC_VECTOR(mult_signed(2*Nb-1 DOWNTO Nb));
-END beh;
+	mult_out <= STD_LOGIC_VECTOR(mult_signed);
+END beh_mult;
