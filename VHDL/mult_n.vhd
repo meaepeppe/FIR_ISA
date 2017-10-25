ef4c2e0fd6d0d9ba14c4b421129973b3311474c3
GuidoBaccelli <32876385+GuidoBaccelli@users.noreply.github.com>
2017-10-25 21:30:28 +0200

    Add files via upload

--- VHDL/mult_n.vhd
+++ VHDL/mult_n.vhd
@@ -3,9 +3,7 @@ USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
 
 ENTITY mult_n IS
-	GENERIC(
-		Nb: INTEGER := 9
-	);
+	GENERIC(Nb: INTEGER := 9);
 	PORT(
 		in_a: IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
 		in_b: IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
