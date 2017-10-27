90a06d0e6b2c3af623b123d2bea3b62bdd9a489b
GuidoBaccelli <32876385+GuidoBaccelli@users.noreply.github.com>
2017-10-27 12:32:25 +0200

    Add files via upload

--- VHDL/Cell_Pipe.vhd
+++ VHDL/Cell_Pipe.vhd
@@ -4,8 +4,7 @@ USE ieee.numeric_std.all;
 
 ENTITY Cell_Pipe IS 
 	GENERIC(Nb:INTEGER:=9;
-			Ord: INTEGER := 8;
-			NRegs: INTEGER := 1); -- Num of Pipeline Registers
+			Ord: INTEGER := 8); 
 	PORT(
 		CLK, RST_n, EN : IN STD_LOGIC;
 		DIN : IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
@@ -18,10 +17,7 @@ END ENTITY;
 
 ARCHITECTURE beh OF Cell_Pipe IS
 
-	TYPE Reg_sig_array IS ARRAY (NRegs-1 DOWNTO 0) OF STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
-	SIGNAL Regs_pipe_out: Reg_sig_array;
-	
-	SIGNAL mult, Last_reg_out, Last_reg_ext: STD_LOGIC_VECTOR(2*Nb-1 DOWNTO 0);
+	SIGNAL mult, Last_reg_out, Last_reg_ext, Sum_in_reg: STD_LOGIC_VECTOR(2*Nb-1 DOWNTO 0);
 	SIGNAL Reg_buf: STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
 
 	COMPONENT adder_n IS
@@ -60,26 +56,22 @@ BEGIN
 			   PORT MAP(DIN => DIN, CLK => CLK, RST_n => RST_n, EN => EN, DOUT => Reg_buf);
 	
 	REG_OUT <= Reg_buf;
-	Regs_pipe_out(0) <= Reg_buf;
-	
-	Pipe_Reg_gen: FOR i IN 0 TO (NRegs-2) GENERATE
-		Single_Reg_gen: Reg_n GENERIC MAP(Nb => Nb)
-		PORT MAP(CLK => CLK, RST_n => RST_n, EN => EN,
-		DIN => Regs_pipe_out(i),
-		DOUT => Regs_pipe_out(i+1)
-		);
-	END GENERATE;
 
 	Product: mult_n GENERIC MAP(Nb => Nb)
-					PORT MAP(in_a => Regs_pipe_out(NRegs-1), in_b => Bi, mult_out => mult);
+					PORT MAP(in_a => Reg_buf, in_b => Bi, mult_out => mult);
 					
-	Pipe_reg_last_gen: Reg_n GENERIC MAP(Nb => 2*Nb)
+	Pipe_reg: Reg_n GENERIC MAP(Nb => mult'LENGTH)
 				PORT MAP(DIN => mult, CLK => CLK, RST_n => RST_n, EN => EN, DOUT => Last_reg_out);
 	
 	Last_reg_ext(Nb DOWNTO 0) <= Last_reg_out(Nb+Ord DOWNTO Ord);
 	Last_reg_ext(2*Nb-1 DOWNTO Nb+1) <= (OTHERS => (Last_reg_ext(Nb)));
 	
+	Sum_reg: Reg_n GENERIC MAP(Nb => SUM_IN'LENGTH)
+				   PORT MAP(CLK => CLK, RST_n => RST_n, EN => EN,
+							DIN => SUM_IN,
+							DOUT => Sum_in_reg);
+	
 	Sum: adder_n GENERIC MAP(Nb => Nb+Ord+1)
-				 PORT MAP(in_a => SUM_IN, in_b => Last_reg_ext, sum_out => ADD_OUT);
+				 PORT MAP(in_a => Sum_in_reg, in_b => Last_reg_ext, sum_out => ADD_OUT);
 				
 END beh;	
--- VHDL/FIR_Filter_Pipe.vhd
+++ VHDL/FIR_Filter_Pipe.vhd
@@ -2,53 +2,45 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
 
-ENTITY FIR_Filter_Pipe IS
-	GENERIC(
-			Ord: INTEGER := 8; --Filter Order
-			Nb: INTEGER := 9; --# of bits
-			PO: INTEGER := 3 -- Pipeline Order
-			);
-	PORT(
-		CLK, RST_n:	IN STD_LOGIC;
-		VIN:	IN STD_LOGIC;
-		DIN: IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
-		Coeffs:	IN	STD_LOGIC_VECTOR(((Ord+1)*Nb)-1 DOWNTO 0); --# of coeffs IS N+1
-		VOUT: OUT STD_LOGIC;
-		DOUT: OUT STD_LOGIC_VECTOR(Nb+Ord DOWNTO 0)
-	);
+
+ENTITY FIR_filter_Pipe IS
+GENERIC(
+		Ord: INTEGER := 8; --Filter Order
+		Nb: INTEGER := 9 --# of bits
+		);
+PORT(
+	CLK, RST_n:	IN STD_LOGIC;
+	VIN:	IN STD_LOGIC;
+	DIN : IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
+	Coeffs:	IN	STD_LOGIC_VECTOR(((Ord+1)*Nb)-1 DOWNTO 0); --# of coeffs IS Ord+1
+	VOUT: OUT STD_LOGIC;
+	DOUT:	OUT STD_LOGIC_VECTOR(2*Nb-1 DOWNTO 0)
+	
+);
 END ENTITY;
 
-
-ARCHITECTURE beh OF FIR_Filter_Pipe IS
-
-		
-	TYPE sum_0_3_array IS ARRAY (0 TO 3) OF STD_LOGIC_VECTOR(Ord+Nb DOWNTO 0);
-	TYPE sum_3_6_array IS ARRAY (3 TO 6) OF STD_LOGIC_VECTOR(Ord+Nb DOWNTO 0);
-	TYPE sum_6_8_array IS ARRAY(6 TO 8) OF STD_LOGIC_VECTOR(Ord+Nb DOWNTO 0);
+ARCHITECTURE beh OF FIR_filter_Pipe IS
+	
+	TYPE sum_array IS ARRAY (Ord DOWNTO 0) OF STD_LOGIC_VECTOR(2*Nb-1 DOWNTO 0);
 	TYPE sig_array IS ARRAY (Ord DOWNTO 0) OF STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
-	TYPE Reg_array IS ARRAY (Ord DOWNTO 0) OF STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
 	
 	SIGNAL Bi: sig_array; -- there IS Ord instead of Ord-1 becaUSE the coeffs are Ord+1
 	SIGNAL REG_OUT_array: sig_array;
-	SIGNAL Sum_0_3: sum_0_3_array;
-	SIGNAL Sum_3_6: sum_3_6_array;
-	SIGNAL Sum_6_8: sum_6_8_array;
+	SIGNAL SUM_OUT_array: sum_array;
 	
-	SIGNAL DIN_mult, DIN_mult_ext: STD_LOGIC_VECTOR(Nb+Ord DOWNTO 0);
-	--SIGNAL VIN_Delay: STD_LOGIC_VECTOR(Ord DOWNTO 0); -- PROVVISORIA
-	SIGNAL tmp: STD_LOGIC_VECTOR(Ord+Nb DOWNTO 0);
+	SIGNAL DIN_mult: STD_LOGIC_VECTOR(2*Nb-1 DOWNTO 0);
+	SIGNAL mult_ext: STD_LOGIC_VECTOR(2*Nb-1 DOWNTO 0);
 	
 	COMPONENT Cell_Pipe IS 
 		GENERIC(Nb:INTEGER:=9;
-				Ord: INTEGER := 8;
-				NRegs: INTEGER := 1); -- Num of Pipeline Registers
+				Ord: INTEGER:=8);
 		PORT(
 			CLK, RST_n, EN : IN STD_LOGIC;
 			DIN : IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
 			SUM_IN: IN STD_LOGIC_VECTOR(Nb+Ord DOWNTO 0);
 			Bi: IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
-			REG_OUT : BUFFER STD_LOGIC_VECTOR(Nb-1 DOWNTO 0); 
-			ADD_OUT: OUT STD_LOGIC_VECTOR(Nb+Ord DOWNTO 0) -- ADD_OUT has one more bit than the inputs
+			REG_OUT : OUT STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
+			ADD_OUT: OUT STD_LOGIC_VECTOR(Nb+Ord DOWNTO 0)
 		);
 	END COMPONENT;
 	
@@ -73,68 +65,35 @@ ARCHITECTURE beh OF FIR_Filter_Pipe IS
 	END COMPONENT;
 
 BEGIN
-	-- DOUT Generation
 	
 	Coeff_gen: FOR i IN 0 to Ord GENERATE
 		Bi(i) <= Coeffs(((i+1)*Nb)-1 DOWNTO (i*Nb));
 	END GENERATE;
 	
-	REG_OUT_array(0) <= DIN;
-	
 	DIN_mult_gen: mult_n GENERIC MAP(Nb => Nb)
 						 PORT MAP(in_a => DIN, in_b => Bi(0), mult_out => DIN_mult);	
 	
-	DIN_mult_ext(Nb DOWNTO 0) <= DIN_mult(Nb+Ord DOWNTO Ord);
-	DIN_mult_ext(Nb+Ord DOWNTO Nb+1) <= ( OTHERS => DIN_mult_ext(Nb));
+	REG_OUT_array(0) <= DIN;
 	
+	mult_ext(Nb DOWNTO 0) <= DIN_mult (Nb+Ord DOWNTO Ord);
+	mult_ext(2*Nb-1 DOWNTO Nb+1) <= (others => mult_ext(Nb));
+
+	SUM_OUT_array(0) <= mult_ext;
 	
-	DIN_Mult_Pipe: Reg_n GENERIC MAP( Nb => 2*Nb)
-						PORT MAP(CLK => CLK, RST_n => RST_n, EN => VIN,
-						DIN => DIN_mult_ext,
-						DOUT => Sum_0_3(0));
-	
-	Cells_gen_1: FOR j IN 0 to 2 GENERATE
-			Single_cell: Cell_Pipe GENERIC MAP(Nb => Nb, Ord => Ord, NRegs => 1) -- Nb is the # of bits entering the j-th cell
+	Cells_gen: FOR j IN 0 to Ord-1 GENERATE
+			Single_cell: Cell_Pipe GENERIC MAP(Nb => Nb, Ord => Ord) -- Nb is the # of bits entering the j-th cell
 						PORT MAP(CLK => CLK, RST_n => RST_n, EN => VIN,
 									DIN => REG_OUT_array(j),
-									SUM_IN => Sum_0_3(j), 
+									SUM_IN => SUM_OUT_array(j), 
 									Bi => Bi(j+1), 
 									REG_OUT => REG_OUT_array(j+1),
-									ADD_OUT => Sum_0_3(j+1));
+									ADD_OUT => SUM_OUT_array(j+1));
 	END GENERATE;
 	
-	ADD_Pipe_1: Reg_n GENERIC MAP(Nb => Ord+Nb+1)
-	PORT MAP(CLK => CLK, RST_n => RST_n, EN => VIN,
-		DIN => Sum_0_3(3),
-		DOUT => Sum_3_6(3));
-	
-	Cells_gen_2: FOR j IN 3 to 5 GENERATE
-			Single_cell: Cell_Pipe GENERIC MAP(Nb => Nb, Ord => Ord, NRegs => 2) -- Nb is the # of bits entering the j-th cell
-						PORT MAP(CLK => CLK, RST_n => RST_n, EN => VIN,
-									DIN => REG_OUT_array(j),
-									SUM_IN => Sum_3_6(j), 
-									Bi => Bi(j+1), 
-									REG_OUT => REG_OUT_array(j+1),
-									ADD_OUT => Sum_3_6(j+1));
-	END GENERATE;
-	
-	ADD_Pipe_2: Reg_n GENERIC MAP(Nb => Ord+Nb+1)
-	PORT MAP(CLK => CLK, RST_n => RST_n, EN => VIN,
-		DIN => Sum_3_6(6),
-		DOUT => Sum_6_8(6));
-	
-	Cells_gen_3: FOR j IN 6 to 7 GENERATE
-			Single_cell: Cell_Pipe GENERIC MAP(Nb => Nb, Ord => Ord, NRegs => 3) -- Nb is the # of bits entering the j-th cell
-						PORT MAP(CLK => CLK, RST_n => RST_n, EN => VIN,
-									DIN => REG_OUT_array(j),
-									SUM_IN => Sum_6_8(j), 
-									Bi => Bi(j+1), 
-									REG_OUT => REG_OUT_array(j+1),
-									ADD_OUT => Sum_6_8(j+1));
-	END GENERATE;
-	
-	DOUT <= Sum_6_8(Ord);
+	DOUT <= SUM_OUT_array(Ord);
 	
 	VOUT <= VIN;
+	
+END beh;
 
-END ARCHITECTURE;
+
