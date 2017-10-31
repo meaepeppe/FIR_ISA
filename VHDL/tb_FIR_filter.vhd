96fe5d42906d12d1f7f82f4c046dd47ea3b7e4bd
GuidoBaccelli <32876385+GuidoBaccelli@users.noreply.github.com>
2017-10-31 12:32:29 +0100

    Add files via upload

--- VHDL/tb_FIR_filter.vhd
+++ VHDL/tb_FIR_filter.vhd
@@ -7,44 +7,54 @@ ENTITY tb_FIR_filter IS
 GENERIC(
 	N: integer := 8;
 	Nb: integer := 9;
-	N_sample: integer := 1000
+	N_sample: integer := 201
 );
 END ENTITY;
 
 ARCHITECTURE test OF tb_FIR_filter IS
 
+	TYPE sample_sign_array IS ARRAY (2 DOWNTO 0) OF SIGNED(Nb-1 DOWNTO 0);
 	TYPE vector_test IS ARRAY (N_sample-1 DOWNTO 0) OF INTEGER;
 	TYPE coeffs_array IS ARRAY (N DOWNTO 0) OF INTEGER;
 	TYPE sig_array IS ARRAY (N DOWNTO 0) OF SIGNED(Nb-1 DOWNTO 0);
+	TYPE sample_std_array IS ARRAY (2 DOWNTO 0) OF STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
+	TYPE sampleout_std_array IS ARRAY (2 DOWNTO 0) OF STD_LOGIC_VECTOR(2*Nb-1 DOWNTO 0);
 
 	FILE inputs: text;
 	FILE coeff_file: text;
+	FILE results: text;
 	SHARED VARIABLE input_samples: vector_test;
 
 	SIGNAL CLK, RST_n: STD_LOGIC;
 	SIGNAL VIN, VOUT: STD_LOGIC;
-	SIGNAL sample: SIGNED(Nb-1 DOWNTO 0);
-	SIGNAL DINconverted: STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
-	SIGNAL filter_out: STD_LOGIC_VECTOR(2*Nb-1 DOWNTO 0);
+	SIGNAL sample: sample_sign_array;
+	SIGNAL DINconverted: sample_std_array;
+	SIGNAL filter_out: sampleout_std_array;
 	SIGNAL coeffs_std: std_logic_vector ((N+1)*Nb - 1 DOWNTO 0);
 	SIGNAL visual_coeffs_integer: coeffs_array;
 	
-	SIGNAL regToDIN: STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
-	SIGNAL DOUTtoReg: STD_LOGIC_VECTOR(2*Nb-1 DOWNTO 0);
+	SIGNAL VIN_array: STD_LOGIC_VECTOR(2 DOWNTO 0);
+		
+	SIGNAL regToDIN: sample_std_array;
+	SIGNAL DOUTtoReg: sampleout_std_array;
 	
-	COMPONENT FIR_filter_Pipe IS
+	COMPONENT FIR_filter_Unf_Pipe IS
 	GENERIC(
 			Ord: INTEGER := 8; --Filter Order
 			Nb: INTEGER := 9; --# of bits
-			PO: INTEGER := 3 -- Pipeline Order
+			UO: INTEGER := 3 -- Unfolding Order
 			);
 	PORT(
 		CLK, RST_n:	IN STD_LOGIC;
 		VIN:	IN STD_LOGIC;
-		DIN: IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
+		DIN_0 : IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
+		DIN_1 : IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
+		DIN_2 : IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
 		Coeffs:	IN	STD_LOGIC_VECTOR(((Ord+1)*Nb)-1 DOWNTO 0); --# of coeffs IS N+1
 		VOUT: OUT STD_LOGIC;
-		DOUT: OUT STD_LOGIC_VECTOR(2*Nb-1 DOWNTO 0)
+		DOUT_0: OUT STD_LOGIC_VECTOR(Nb+Ord DOWNTO 0);
+		DOUT_1: OUT STD_LOGIC_VECTOR(Nb+Ord DOWNTO 0);
+		DOUT_2: OUT STD_LOGIC_VECTOR(Nb+Ord DOWNTO 0)
 	);
 	END COMPONENT;
 	
@@ -59,21 +69,37 @@ ARCHITECTURE test OF tb_FIR_filter IS
 
 	
 BEGIN
-DINconverted <= std_logic_vector(sample);
 
-DUT: FIR_filter_Pipe 
-	PORT MAP (CLK => CLK, RST_n => RST_n, VIN => VIN, DIN => regToDIN,  
-						Coeffs => coeffs_std, VOUT => VOUT, DOUT => DOUTtoReg);
-	
-REG_IN: Reg_n 
-	GENERIC MAP (Nb => Nb)
-	PORT MAP (CLK => CLK, RST_n => RST_n, EN => VIN, DIN => DINconverted, DOUT => regToDIN );
+DINconverted(0) <= std_logic_vector(sample(0));
+DINconverted(1) <= std_logic_vector(sample(1));
+DINconverted(2) <= std_logic_vector(sample(2));
 
-REG_OUT: Reg_n
-	GENERIC MAP (Nb => 2*Nb)
-	PORT MAP (CLK => CLK, RST_n => RST_n, EN => VIN, DIN => DOUTtoReg, DOUT => filter_out );
+VIN_array(0) <= VIN;
+
+VIN_REGS:	FOR i IN 0 TO 1 GENERATE
+	REGS_VIN: Reg_n
+		GENERIC MAP (Nb => 1)
+		PORT MAP(CLK => CLK, RST_n => RST_n, EN => '1', DIN => VIN_array(i DOWNTO i), DOUT => VIN_array(i+1 DOWNTO i+1));
+	END GENERATE;
+
+DUT: FIR_filter_Unf_Pipe
+	PORT MAP (CLK => CLK, RST_n => RST_n, VIN => VIN_array(2), DIN_0 => regToDIN(0),  
+						DIN_1 => regToDIN(1), DIN_2 => regToDIN(2), 
+						Coeffs => coeffs_std, VOUT => VOUT, DOUT_0 => DOUTtoReg(0),
+						DOUT_1 => DOUTtoReg(1), DOUT_2 => DOUTtoReg(2));
 	
-	CLK_gen: PROCESS
+in_reg_layer: FOR i IN 0 TO 2 GENERATE
+				REG_IN: Reg_n 
+					GENERIC MAP (Nb => Nb)
+					PORT MAP (CLK => CLK, RST_n => RST_n, EN => VIN_array(1), DIN => DINconverted(i), DOUT => regToDIN(i) );
+				END GENERATE;
+out_reg_layer: FOR i IN 0 TO 2 GENERATE
+				REG_OUT: Reg_n
+					GENERIC MAP (Nb => 2*Nb)
+					PORT MAP (CLK => CLK, RST_n => RST_n, EN => VOUT, DIN => DOUTtoReg(i), DOUT => filter_out(i) );
+				END GENERATE;
+	
+CLK_gen: PROCESS
 	BEGIN
 		CLK <= '0';
 		WAIT FOR 10 ns;
@@ -81,15 +107,14 @@ REG_OUT: Reg_n
 		WAIT FOR 10 ns;
 	END PROCESS;
 		
-	test_input_read: PROCESS
+test_input_read: PROCESS
 	VARIABLE iLine,cLine: LINE;
 	VARIABLE i,j: INTEGER := 0;
 	VARIABLE coeffs_integer: coeffs_array;
-
 	BEGIN
 	VIN <= '0';
 	RST_n <= '0';
-		file_open(inputs, "input_vectors.txt", READ_MODE);
+		file_open(inputs, "samples.txt", READ_MODE);
 		WHILE (NOT ENDFILE(inputs)) LOOP
 			READLINE(inputs, iLine);
 			READ(iLine, input_samples(i));
@@ -113,26 +138,44 @@ REG_OUT: Reg_n
 		WAIT FOR 5 ns;
 		VIN <= '1';
 		
+		WAIT FOR 745 ns;
+		VIN <= '0';
+		WAIT FOR 60 ns;
+		VIN <= '1';
+		WAIT FOR 600 ns;
+		VIN <= '0';
+		
+		--WAIT FOR 1345 ns;
+		--VIN <= '0';
 		WAIT;
 	
 	END PROCESS;
 	
-	test_results_write: PROCESS(CLK)
+test_results_write: PROCESS(CLK)
 	VARIABLE oLine: LINE;
 	VARIABLE i: INTEGER := 0;
-	FILE results: text is out "output_vectors.txt";
 	BEGIN
+		file_open(results, "output_vectors.txt", WRITE_MODE);
 		IF CLK'EVENT AND CLK = '1' THEN
-			sample <= to_signed(input_samples(i),sample'LENGTH);
-			i:= i+1;
+			IF VIN = '1' THEN
+				sample(0) <= to_signed(input_samples(i),sample(0)'LENGTH);
+				sample(1) <= to_signed(input_samples(i+1),sample(1)'LENGTH);
+				sample(2) <= to_signed(input_samples(i+2),sample(2)'LENGTH);
+				i:= i+3;
+			END IF;
 		END IF;
 		IF CLK'EVENT AND CLK = '1' AND VIN = '1' THEN
-			WRITE(oLine, to_integer(signed(filter_out)));
+			WRITE(oLine, to_integer(signed(filter_out(0))));
+			WRITELINE(results, oLine);
+			WRITE(oLine, to_integer(signed(filter_out(1))));
+			WRITELINE(results, oLine);
+			WRITE(oLine, to_integer(signed(filter_out(2))));
 			WRITELINE(results, oLine);
 			
-			--IF i = N_sample-1 THEN
-			--	VIN <= '0';
-			--END IF;
+		--IF i = N_sample-1 THEN
+		--	VIN <= '0';
+		--END IF;
+		
 		END IF;
 		
 	END PROCESS;
