c7b0a74c8c8cedb1310deb5bb35833e09fdd915a
GuidoBaccelli <32876385+GuidoBaccelli@users.noreply.github.com>
2017-12-30 22:07:38 +0200

    FIR Direct Form final version

--- VHDL/Cell.vhd
+++ VHDL/Cell.vhd
@@ -1,24 +1,31 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
+USE ieee.math_real.all;
 
-ENTITY Cell IS 
+
+
+ENTITY Cell IS
 	GENERIC(Nb:INTEGER:=9;
-			Ord: INTEGER := 8); -- Filter Order
+			Ord: INTEGER := 8; -- Filter Order
+			Nbmult: INTEGER := 10
+			);
 	PORT(
 		CLK, RST_n, EN : IN STD_LOGIC;
 		DIN : IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
-		SUM_IN: IN STD_LOGIC_VECTOR(Nb+Ord DOWNTO 0);
+		SUM_IN: IN STD_LOGIC_VECTOR((Nbmult+1+integer(ceil(log2(real(Ord)))))-1 DOWNTO 0);
 		Bi: IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
 		REG_OUT : OUT STD_LOGIC_VECTOR(Nb-1 DOWNTO 0); 
-		ADD_OUT: OUT STD_LOGIC_VECTOR(Nb+Ord DOWNTO 0) -- aggiunti 9 bit di guardia
+		ADD_OUT: OUT STD_LOGIC_VECTOR((Nbmult+1+integer(ceil(log2(real(Ord)))))-1 DOWNTO 0) -- aggiunti 9 bit di guardia
 	);
 END ENTITY;
 
 ARCHITECTURE beh_cell OF Cell IS
 
+	CONSTANT Nbadder : INTEGER := Nbmult+1+integer(ceil(log2(real(Ord))));
+
 	SIGNAL mult: STD_LOGIC_VECTOR(2*Nb-1 DOWNTO 0);
-	SIGNAL mult_ext: STD_LOGIC_VECTOR(2*Nb-1 DOWNTO 0);
+	SIGNAL mult_ext: STD_LOGIC_VECTOR(Nbadder-1 DOWNTO 0);
 	SIGNAL REG_OUT_sig: STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
 
 	COMPONENT adder_n IS
@@ -60,10 +67,10 @@ BEGIN
 	Product: mult_n GENERIC MAP(Nb => Nb)
 					PORT MAP(in_a => REG_OUT_sig, in_b => Bi, mult_out => mult);
 	
-	mult_ext(Nb+1 DOWNTO 0) <= mult (Nb+Ord DOWNTO Ord-1);
-	mult_ext(2*Nb-1 DOWNTO Nb+2) <= (others => mult_ext(Nb+1));
+	mult_ext(Nbmult-1 DOWNTO 0) <= mult ((mult'LENGTH)-1 DOWNTO (mult'LENGTH)-1-(Nbmult-1));
+	mult_ext(Nbadder-1 DOWNTO Nbmult) <= (others => mult_ext(Nbmult-1));
 	
-	Sum: adder_n GENERIC MAP(Nb => Nb+Ord+1) -- aggiunti 9 bit di guardia
+	Sum: adder_n GENERIC MAP(Nb => Nbadder) -- aggiunti 9 bit di guardia
 				 PORT MAP(in_a => SUM_IN, in_b => mult_ext, sum_out => ADD_OUT);
 				
 END beh_cell;	
--- VHDL/FIR_filter.vhd
+++ VHDL/FIR_filter.vhd
@@ -1,12 +1,13 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.numeric_std.all;
-
+USE ieee.math_real.all;
 
 ENTITY FIR_filter IS
 GENERIC(
 		Ord: INTEGER := 8; --Filter Order
-		Nb: INTEGER := 9 --# of bits
+		Nb: INTEGER := 9; --# of bits
+		Nbmult: INTEGER := 10 -- # of significant bits from the multiplier
 		);
 PORT(
 	CLK, RST_n:	IN STD_LOGIC;
@@ -14,14 +15,18 @@ PORT(
 	DIN : IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
 	Coeffs:	IN	STD_LOGIC_VECTOR(((Ord+1)*Nb)-1 DOWNTO 0); --# of coeffs IS Ord+1
 	VOUT: OUT STD_LOGIC;
-	DOUT:	OUT STD_LOGIC_VECTOR(2*Nb-1 DOWNTO 0)
+	DOUT:	OUT STD_LOGIC_VECTOR(Nb-1 DOWNTO 0)
 	
 );
 END ENTITY;
 
 ARCHITECTURE beh_fir OF FIR_filter IS
 	
-	TYPE sum_array IS ARRAY (Ord DOWNTO 0) OF STD_LOGIC_VECTOR(2*Nb-1 DOWNTO 0);
+	--CONSTANT Nbadder :INTEGER := Nb + integer(ceil(log2(Ord+1)));
+	
+	CONSTANT Nbadder: INTEGER := Nbmult+1+integer(ceil(log2(real(Ord))));
+	
+	TYPE sum_array IS ARRAY (Ord DOWNTO 0) OF STD_LOGIC_VECTOR(Nbadder-1 DOWNTO 0);
 	TYPE sig_array IS ARRAY (Ord DOWNTO 0) OF STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
 	
 	SIGNAL Bi: sig_array; -- there IS Ord instead of Ord-1 becaUSE the coeffs are Ord+1
@@ -29,21 +34,23 @@ ARCHITECTURE beh_fir OF FIR_filter IS
 	SIGNAL SUM_OUT_array: sum_array;
 	
 	SIGNAL DIN_mult: STD_LOGIC_VECTOR(2*Nb-1 DOWNTO 0);
-	SIGNAL mult_ext: STD_LOGIC_VECTOR(2*Nb-1 DOWNTO 0);
+	SIGNAL mult_ext: STD_LOGIC_VECTOR(Nbadder-1 DOWNTO 0);
 	
-	COMPONENT Cell IS 
+	COMPONENT Cell IS
 		GENERIC(Nb:INTEGER:=9;
-				Ord: INTEGER:=8);
+				Ord: INTEGER := 8; -- Filter Order
+				Nbmult: INTEGER := 10
+				);
 		PORT(
 			CLK, RST_n, EN : IN STD_LOGIC;
 			DIN : IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
-			SUM_IN: IN STD_LOGIC_VECTOR(Nb+Ord DOWNTO 0);
+			SUM_IN: IN STD_LOGIC_VECTOR((Nbmult+1+integer(ceil(log2(real(Ord)))))-1 DOWNTO 0);
 			Bi: IN STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
-			REG_OUT : OUT STD_LOGIC_VECTOR(Nb-1 DOWNTO 0);
-			ADD_OUT: OUT STD_LOGIC_VECTOR(Nb+Ord DOWNTO 0)
+			REG_OUT : OUT STD_LOGIC_VECTOR(Nb-1 DOWNTO 0); 
+			ADD_OUT: OUT STD_LOGIC_VECTOR((Nbmult+1+integer(ceil(log2(real(Ord)))))-1 DOWNTO 0) -- aggiunti 9 bit di guardia
 		);
 	END COMPONENT;
-	
+
 	COMPONENT Reg_n IS
 		GENERIC(Nb: INTEGER :=9);
 		PORT(
@@ -76,13 +83,13 @@ BEGIN
 	
 	REG_OUT_array(0) <= DIN;
 	
-	mult_ext(Nb+1 DOWNTO 0) <= DIN_mult (Nb+Ord DOWNTO Ord-1);
-	mult_ext(2*Nb-1 DOWNTO Nb+2) <= (others => mult_ext(Nb+1));
+	mult_ext(Nbmult-1 DOWNTO 0) <= DIN_mult((DIN_mult'LENGTH)-1 DOWNTO (DIN_mult'LENGTH)-1-(Nbmult-1));
+	mult_ext(Nbadder-1 DOWNTO Nbmult) <= (others => mult_ext(Nbmult-1));
 
 	SUM_OUT_array(0) <= mult_ext;
 	
 	Cells_gen: FOR j IN 0 to Ord-1 GENERATE
-			Single_cell: Cell GENERIC MAP(Nb => Nb, Ord => Ord) -- Nb is the # of bits entering the j-th cell
+			Single_cell: Cell GENERIC MAP(Nb => Nb, Ord => Ord, Nbmult => Nbmult) -- Nb is the # of bits entering the j-th cell
 						PORT MAP(CLK => CLK, RST_n => RST_n, EN => VIN,
 									DIN => REG_OUT_array(j),
 									SUM_IN => SUM_OUT_array(j), 
@@ -91,8 +98,8 @@ BEGIN
 									ADD_OUT => SUM_OUT_array(j+1));
 	END GENERATE;
 	
-	DOUT <= SUM_OUT_array(Ord);
-	
+	--DOUT <= SUM_OUT_array(Ord)(Nbadder-1 DOWNTO (Nbadder-1)-(Nb-1));
+		DOUT <= SUM_OUT_array(Ord)(Nb-1 DOWNTO 0);
 	VOUT <= VIN;
 	
 END beh_fir;
